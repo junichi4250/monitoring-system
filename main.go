74825150cea3c1e24fe761f2c0479983140d3de9
katojun <jkky0225@gmail.com>
2022-08-07 01:03:21 +0900

    varを削除

--- main.go
+++ main.go
@@ -5,11 +5,9 @@ import (
 )
 
 func main() {
-	var (
-		N = flag.Int("timeout", 5, "timeout flag") // timeout回数
-		m = flag.Int("responseTimeCount", 2, "responseTimeCount flag") // 平均応答時間を計測する回数
-		t = flag.Int("averageResponseTime", 10, "averageResponseTime flag") // 平均応答時間
-	)
+	N := flag.Int("timeout", 5, "timeout flag") // timeout回数
+	m := flag.Int("responseTimeCount", 2, "responseTimeCount flag") // 平均応答時間を計測する回数
+	t := flag.Int("averageResponseTime", 10, "averageResponseTime flag") // 平均応答時間
 	flag.Parse()
 
 	breakServer := []*Server{}
