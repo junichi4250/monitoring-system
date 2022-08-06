203fcebb3e73ba3167b998deec67aa37dc5694c2
katojun <jkky0225@gmail.com>
2022-08-06 19:41:15 +0900

    n回以上のタイムアウトを設定

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -14,9 +15,13 @@ type Server struct {
 	ip string
 	responseTime string
 	breakTime int
+	timeoutCount int
 }
 
 func main() {
+	var N = flag.Int("timeout", 5, "timeout flag")
+	flag.Parse()
+
 	data, _ :=  os.Open("access.log")
 	defer data.Close()
 	scanner := bufio.NewScanner(data)
@@ -30,12 +35,13 @@ func main() {
 				strings.Split(scanner.Text(), ",")[1],
 				strings.Split(scanner.Text(), ",")[2],
 				0,
+				0,
 			})
 		}
 
 		// pingを送るサーバーが故障していたものかチェック
 		index := include(server, strings.Split(scanner.Text(), ",")[1])
-		if index != -1 && server[index].breakTime == 0 {
+		if index != -1 {
 			if !strings.HasSuffix(scanner.Text(), "-") {
 				// 故障が改善していれば故障期間を測定
 				recordStartTime, err := strconv.Atoi(server[index].recordTime)
@@ -47,11 +53,23 @@ func main() {
 					log.Fatal(err)
 				}
 				server[index].breakTime = recordEndTime - recordStartTime
+			} else {
+				// 故障が改善していなければtimeout回数を増やす
+				server[index].timeoutCount++
+				if server[index].timeoutCount >= *N {
+					// n回以上タイムアウトの時は故障
+					fmt.Println(*N, "回以上タイムアウトしました。")
+					fmt.Println("ipアドレス:", server[index].ip)
+				}
 			}
 		}
 	}
 
 	for _, v := range server {
+		// 故障期間が0になる場合は続けて2回以上timeoutした時なので表示からは除く
+		if v.breakTime == 0 {
+			continue
+		}
 		// 故障しているサーバーのipアドレス表示
 		fmt.Println("故障サーバーip:" , v.ip)
 		// 故障期間の出力
